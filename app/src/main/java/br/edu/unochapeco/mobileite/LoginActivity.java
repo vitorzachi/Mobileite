c3660b1135707a97893c270d7ecc31634639dee6
vitor.zachi <vitor.zachi@camtwo.com.br>
2015-06-05 20:27:45 -0300

    modificado para conseguir fazer login

--- app/src/main/java/br/edu/unochapeco/mobileite/LoginActivity.java
+++ app/src/main/java/br/edu/unochapeco/mobileite/LoginActivity.java
@@ -5,21 +5,24 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import br.edu.unochapeco.mobileite.constantes.Constantes;
 import br.edu.unochapeco.mobileite.service.LoginService;
 
 
-public class LoginActivity extends ActionBarActivity implements ServiceConnection {
-    private LoginService loginService;
+public class LoginActivity extends ActionBarActivity  {
+
     private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
     private EditText txtCodigo, txtCpf;
@@ -35,6 +38,7 @@ public class LoginActivity extends ActionBarActivity implements ServiceConnectio
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         txtCodigo = (EditText) findViewById(R.id.txtCodigo);
         txtCpf = (EditText) findViewById(R.id.txtCpf);
 
@@ -47,20 +51,10 @@ public class LoginActivity extends ActionBarActivity implements ServiceConnectio
         fazLogin(codigo, cpf);
     }
 
-    private void vaiParaHome() {
-        Intent intent = new Intent(this, HomeActivity.class);
-        startActivity(intent);
-        finish();
-    }
+
 
     private void fazLogin(String codigo, String cpf) {
-        Intent intent = new Intent(this, LoginService.class);
-        bindService(intent, this, Context.BIND_AUTO_CREATE);
-        boolean loginSucesso = loginService.doLogin(cpf, codigo);
-
-        if(loginSucesso){
-            vaiParaHome();
-        }
+        new LoginTask(cpf, codigo).execute();
     }
 
     @Override
@@ -82,15 +76,7 @@ public class LoginActivity extends ActionBarActivity implements ServiceConnectio
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onServiceConnected(ComponentName name, IBinder service) {
-        this.loginService = ((LoginService.LoginBinder) service).getService();
-    }
 
-    @Override
-    public void onServiceDisconnected(ComponentName name) {
-
-    }
 
     public void entrar(View view) {
         txtCodigo = (EditText) findViewById(R.id.txtCodigo);
@@ -98,4 +84,29 @@ public class LoginActivity extends ActionBarActivity implements ServiceConnectio
 
         fazLogin(txtCodigo.getText().toString(), txtCpf.getText().toString());
     }
+
+    private class LoginTask extends AsyncTask<String, Void, Boolean>{
+        private final String cpf, codigo;
+        public LoginTask( String cpf, String codigo) {
+            this.cpf = cpf;
+            this.codigo = codigo;
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            return new LoginService(cpf, codigo).validaLogin();
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            if (aBoolean){
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }else{
+                Toast.makeText(LoginActivity.this, R.string.login_erro, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
 }
--- app/src/main/java/br/edu/unochapeco/mobileite/service/LoginService.java
+++ app/src/main/java/br/edu/unochapeco/mobileite/service/LoginService.java
@@ -1,10 +1,6 @@
 package br.edu.unochapeco.mobileite.service;
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.Binder;
-import android.os.IBinder;
-import android.widget.Toast;
+import android.os.AsyncTask;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,47 +8,41 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 
-import br.edu.unochapeco.mobileite.R;
 import br.edu.unochapeco.mobileite.constantes.Constantes;
 
 /**
  * Created by vitor on 28/05/15.
  */
-public class LoginService extends Service {
-    private Intent intent;
+public class LoginService  {
+    private final String cpf, codigo;
+    private boolean ok=false;
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        this.intent = intent;
-        return new LoginBinder();
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
+    public LoginService( String cpf, String codigo) {
+        this.cpf = cpf;
+        this.codigo = codigo;
     }
 
 
-    public boolean doLogin(String cpf, String codigo) {
+    public boolean validaLogin() {
+        if("".equals(cpf)||"".equals(codigo)){
+            return  false;
+        }
+
         URL url = Constantes.getUrlLogin(cpf, codigo);
         URLConnection conection = null;
-        try {
-            conection = url.openConnection();
-            conection.connect();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
-                    conection.getInputStream()));
 
-            return Boolean.valueOf(reader.readLine());
-        } catch (IOException e) {
-            Toast.makeText(this, R.string.login_erro, Toast.LENGTH_LONG).show();
-            return false;
-        }
+            try {
+                conection = url.openConnection();
+                conection.connect();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        conection.getInputStream()));
+
+                ok = Boolean.valueOf(reader.readLine());
+            } catch (IOException e) {
+                ok = false;
+            }
+
+       return ok;
     }
 
-   public class LoginBinder extends Binder {
-
-        public LoginService getService() {
-            return LoginService.this;
-        }
-    }
 }
